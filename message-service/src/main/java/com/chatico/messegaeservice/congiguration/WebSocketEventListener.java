22da8e71d14a0a762044763dd421a2f7868be976
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-07-19 03:39:58 +0300

    WebSocketEventListener start config

--- message-service/src/main/java/com/chatico/messegaeservice/congiguration/WebSocketEventListener.java
+++ message-service/src/main/java/com/chatico/messegaeservice/congiguration/WebSocketEventListener.java
@@ -1,7 +1,8 @@
 package com.chatico.messegaeservice.congiguration;
 
-import com.alibou.websocket.chat.ChatMessage;
-import com.alibou.websocket.chat.MessageType;
+
+import com.chatico.messegaeservice.domain.ChatMessage;
+import com.chatico.messegaeservice.domain.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
